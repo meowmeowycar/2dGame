2a7224799bb5e9a9e8697d494d33bf7362d6fa75
Maks <166073630+Maks92384@users.noreply.github.com>
2025-04-15 21:03:33 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <SFML/Graphics.hpp>
 #include "events.h"
 #include "ImageDisplay.h"
-  int main()
-{
+
+int main() {
     sf:: Texture backgroundTexture;
     if (!Obrazek("../../Media/Ludzik.png", backgroundTexture)) {
         return -1;
     }
-    
+
     sf::Sprite Spirite(backgroundTexture);
-  
-  auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Game", sf::State::Fullscreen);
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Game", sf::State::Fullscreen);
     window.setFramerateLimit(144);
-  
+
 
     while (window.isOpen())
     {
