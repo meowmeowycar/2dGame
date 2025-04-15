73b032e1c8a2ea3d8f3159a39ac9d71d8c38ab9a
Maks <166073630+Maks92384@users.noreply.github.com>
2025-04-15 20:28:53 +0200

    DeletedCMakeFiles

--- cmake-build-debug/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.obj.d
+++
@@ -1,5 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.obj: \
- C:\Users\agnie\Documents\GitHub\2dGame\cmake-build-debug\_deps\sfml-src\src\SFML\Graphics\StencilMode.cpp \
- C:/Users/agnie/Documents/GitHub/2dGame/cmake-build-debug/_deps/sfml-src/include/SFML/Graphics/StencilMode.hpp \
- C:/Users/agnie/Documents/GitHub/2dGame/cmake-build-debug/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- C:/Users/agnie/Documents/GitHub/2dGame/cmake-build-debug/_deps/sfml-src/include/SFML/Config.hpp
